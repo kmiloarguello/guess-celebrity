86984f030969a5a63f4653a417906900bef5c009
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-08-31 14:39:44 -0500

    Getting image onClick

--- app/src/main/java/xyz/camiloarguello/guessthecelebrity/MainActivity.java
+++ app/src/main/java/xyz/camiloarguello/guessthecelebrity/MainActivity.java
@@ -1,19 +1,32 @@
 package xyz.camiloarguello.guessthecelebrity;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView downloaded;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String r = null;
 
+        downloaded = findViewById(R.id.imageView);
+
+        //https://camiloarguello.xyz/img/compa_juego.png
+
+        /**
+         * To download web code
+         */
+        /*
+        String r = null;
         DownloadTask task = new DownloadTask();
         try {
 
@@ -30,6 +43,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Log.i("Content Resultado", r);
+        */
+
+    }
+
+    public void downloadImage(View view){
+
+        DownloadImage task = new DownloadImage();
+        Bitmap myImage;
+
+        try {
+            myImage = task.execute("https://camiloarguello.xyz/img/compa_juego.png").get();
+
+            downloaded.setImageBitmap(myImage);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
     }
 }
